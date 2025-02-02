da09a41bb2acab2cc81423621f0739c54ffe1975
Jangchaeyun <zzangsally@naver.com>
2025-02-02 16:52:05 +0900

    0202 class component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Hello");
+  }
   state = {
     count: 0,
   };
 
   add = () => {
-    console.log("add");
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
   };
 
   minus = () => {
-    console.log("minus");
+    this.setState((current) => ({ count: current.count - 1 }));
   };
+
+  componentDidMount() {
+    console.log("component rendered");
+  }
+
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+  }
+
   render() {
+    console.log("I'm rendering");
     return (
       <div>
         <h1>The number is {this.state.count}</h1>
