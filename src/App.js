cdf064faeddb4f43908334f5e725884d8ed6d3d5
Jangchaeyun <zzangsally@naver.com>
2025-02-14 14:46:30 +0900

    0214 Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
-import Home from "./routes/Home";
 import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" exact={true} Component={Home} />
+        <Route path="/about" Component={About} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
