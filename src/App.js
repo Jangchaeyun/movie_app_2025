70d6db4bdc154c0f6ae7549d1b102b5ecbd3cb94
Jangchaeyun <zzangsally@naver.com>
2025-02-20 11:57:57 +0900

    0220 MovieDetail

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
 import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" exact={true} Component={Home} />
         <Route path="/about" Component={About} />
+        <Route path="/movie-detail" Component={Detail} />
       </Routes>
     </HashRouter>
   );
